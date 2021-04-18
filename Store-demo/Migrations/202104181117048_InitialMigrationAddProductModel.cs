ec1a36bd372ee2d477975157d70d0f665d523e61
Alex <ardeleanalexdaniel@yahoo.com>
2021-04-18 14:22:16 +0300

    InitializingFirstCodeMigration

---
+++ Store-demo/Migrations/202104181117048_InitialMigrationAddProductModel.cs
@@ -0,0 +1,29 @@
+namespace Store_demo.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialMigrationAddProductModel : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Products",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false),
+                        Code = c.String(nullable: false),
+                        Quantity = c.Long(nullable: false),
+                        Price = c.Single(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Products");
+        }
+    }
+}
--- Store-demo/Models/Product.cs
+++ Store-demo/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,17 @@ namespace Store_demo.Models
 {
     public class Product
     {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Code { get; set; }
+
+        [Required]
+        public long Quantity { get; set; }
+
+        public float Price { get; set; }
     }
 }
